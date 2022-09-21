fc3777f7a00cc57ebbb96143da29ac0f9ea2109e
Duncan Robertson <knacknud@gmail.com>
2022-09-21 15:30:10 +0100

    finished homework and extensions

---
+++ repositories/album_repository.py
@@ -0,0 +1,47 @@
+from db.run_sql import run_sql
+
+from models.album import Album
+
+def save(album):
+    sql = "INSERT INTO albums (title, genre, artist_id) VALUES (%s, %s, %s) RETURNING *"
+    values = [album.title, album.genre, album.artist_id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    album.id = id
+    return album
+
+def select(id):
+    album = None
+    sql = "SELECT * FROM albums WHERE id = %s"
+    values = [id]
+    results = run_sql(sql, values)
+
+    if results:
+        result = results[0]
+        album = Album(result['title'], result['genre'], result['artist_id'], result['id'])
+    return album
+
+def select_all():
+    albums = []
+
+    sql = "SELECT * FROM albums"
+    results = run_sql(sql)
+
+    for row in results:
+        album = Album(row['title'], row['genre'], row['artist_id'], row['id'])
+        albums.append(album)
+    return albums
+
+def update(album):
+    sql = "UPDATE albums SET (title, genre, artist_id) = (%s, %s, %s) WHERE id = %s"
+    values = [album.title, album.genre, album.artist_id, album.id]
+    run_sql(sql, values)
+
+def delete(id):
+    sql = "DELETE FROM albums WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+def delete_all():
+    sql = "DELETE FROM albums"
+    run_sql(sql)
